94777a1a475256b733631e74465f48d21969bcf2
Mitch Richling <11151403+richmit@users.noreply.github.com>
2024-08-04 14:47:52 -0500

    Finished parametric_curve_3d

--- examples/parametric_curve_3d.cpp
+++ examples/parametric_curve_3d.cpp
@@ -29,9 +29,27 @@
   @endparblock
  @filedetails   
 
-  This example illustrates a simple parametric curve in 3D.  Stuff illistrated:
-   - Produce a VTK file representing a parametric curve in 3D.
-   - Produce a VTK file representing a parametric surface in 3D -- actually two VTK files each with a diffrent surface.
+  This program produces an interesting visualization of an object known as the twisted cubic.  In parametric form, the curve may be expressed as
+
+  @f[ f(t)=[t, t^2, t^3] @f]
+
+  Alternately the curve is also the intersection of two surfaces in @f$\mathbb{R}^3@f$:
+
+  @f[ y=f_2(x, z)=y^2 @f]
+  @f[ z=f_3(x, y)=x^3 @f]
+
+  The "typical" way to graph a surface like @f$f_2@f$ is to transform it into pseudo-parametric form.  In Maple that might look like this
+
+  \verbatim                                                                                                              
+  plot3d([u, u^2, v], u=-1..1, v=-1..1):
+  \endverbatim
+
+  We could do that with MRPTree, but it is easier to simply map the variables when we use construct_geometry_fans().
+
+  Another interesting use of MRPTree in this example is the way we have transformed each surface function into an SDF to drive up sample resolution near the
+  surface intersection.  This would allow us to use a tool like Paraview to compute an approximation to the the intersection.  Just in case the reader is
+  not using a tool that can extract a nice surface intersection, I have also dumped the curve out in a 3rd .VTU file.
+
 */
 /*******************************************************************************************************************************************************.H.E.**/
 /** @cond exj */
@@ -46,7 +64,7 @@ typedef mjr::tree15b1d3rT              tt1_t;
 typedef mjr::MRccT5                    cc1_t;
 typedef mjr::MR_rt_to_cc<tt1_t, cc1_t> tc1_t;
 
-typedef mjr::tree15b2d3rT              tt2_t;
+typedef mjr::tree15b2d1rT              tt2_t;
 typedef mjr::MRccT5                    cc2_t;
 typedef mjr::MR_rt_to_cc<tt2_t, cc2_t> tc2_t;
 
@@ -56,18 +74,28 @@ tt1_t::rrpt_t twisted_cubic_crv(tt1_t::drpt_t t) {
 }                          
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-tt2_t::rrpt_t twisted_cubic_srf1(tt2_t::drpt_t uvvec) {
-  tt2_t::src_t u = uvvec[0];
-  tt2_t::src_t v = uvvec[1];
-  return { u, u*u, v };
+tt2_t::rrpt_t twisted_cubic_srf1(tt2_t::drpt_t xzvec) {
+  tt2_t::src_t x = xzvec[0];
+  return x*x;
 }                          
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-tt2_t::rrpt_t twisted_cubic_srf2(tt2_t::drpt_t uvvec) {
-  tt2_t::src_t u = uvvec[0];
-  tt2_t::src_t v = uvvec[1];
-  return { u, v, u*u*u };
+tt2_t::src_t twisted_cubic_srf1_sdf(tt2_t::drpt_t xzvec) {
+  tt2_t::src_t z = xzvec[1];
+  return (twisted_cubic_srf1(xzvec)-z);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+tt2_t::rrpt_t twisted_cubic_srf2(tt2_t::drpt_t xyvec) {
+  tt2_t::src_t x = xyvec[0];
+  return x*x*x;
 }                          
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+tt2_t::src_t twisted_cubic_srf2_sdf(tt2_t::drpt_t xyvec) {
+  tt2_t::src_t y = xyvec[1];
+  return (twisted_cubic_srf2(xyvec)-y);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
@@ -89,12 +117,14 @@ int main() {
   cc2_t srf1_ccplx;
   tc2_t srf1_tree_conv;
   srf1_tree.refine_grid(5, twisted_cubic_srf1);
+  srf1_tree.refine_leaves_recursive_cell_pred(6, twisted_cubic_srf1, [&srf1_tree](tt2_t::diti_t i) { return srf1_tree.cell_cross_sdf(i, twisted_cubic_srf2_sdf); });
+  srf1_tree.balance_tree(1, twisted_cubic_srf1);
   srf1_tree_conv.construct_geometry_fans(srf1_ccplx,
                                          srf1_tree,
                                          2,
-                                         {{tc2_t::tree_val_src_t::RANGE, 0},
-                                          {tc2_t::tree_val_src_t::RANGE, 1},
-                                          {tc2_t::tree_val_src_t::RANGE, 2}});
+                                         {{tc2_t::tree_val_src_t::DOMAIN, 0},
+                                          {tc2_t::tree_val_src_t::RANGE,  0},
+                                          {tc2_t::tree_val_src_t::DOMAIN, 1}});
   srf1_ccplx.create_named_datasets({"u", "v", "x(u,v)", "y(u,v)", "z(u,v)"});
   srf1_ccplx.dump_cplx(5);
   srf1_ccplx.write_xml_vtk("parametric_curve_3d-srf1.vtu", "parametric_curve_3d-srf1");
@@ -103,6 +133,8 @@ int main() {
   cc2_t srf2_ccplx;
   tc2_t srf2_tree_conv;
   srf2_tree.refine_grid(5, twisted_cubic_srf2);
+  srf2_tree.refine_leaves_recursive_cell_pred(6, twisted_cubic_srf2, [&srf2_tree](tt2_t::diti_t i) { return srf2_tree.cell_cross_sdf(i, twisted_cubic_srf1_sdf); });
+  srf2_tree.balance_tree(1, twisted_cubic_srf2);
   srf2_tree_conv.construct_geometry_fans(srf2_ccplx,
                                          srf2_tree,
                                          2,
