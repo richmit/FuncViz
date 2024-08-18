fc1d2473d765e238d64af50d3b6cc16fe965a732
Mitch Richling <11151403+richmit@users.noreply.github.com>
2024-08-18 14:55:20 -0500

    Added check_file_size.sh

---
+++ check_file_size.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Find files too big for github that are not listed in .gitignore
+
+if [ -e paraview -a -e docs/func-viz ]; then
+  find paraview/     -size +50M -a -not -iname '*lossless.webm' -a -not -iname '*crf01.mp4' -exec sh -c "grep -q '^/{}$' .gitignore || echo '{}'" \;
+  find docs/func-viz/ -size +50M
+else
+  echo "ERROR: Run from base of MRPTree repository"
+fi
